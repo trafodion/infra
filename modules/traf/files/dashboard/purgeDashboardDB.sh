3f954822b8bb378b0e17dd2986da43429af6f49b
Alice Chen <alice.chen@hp.com>
2014-05-13 20:18:14 -0700

    Add maintenance for Puppet Dashboard
    Tune MySQL parameters max_allowed_packet, innodb_buffer_pool_size, and
    innodb_flush_method. Patched rake job that cleans up the MySQL database.
    Add daily cron job to do the following maintenance in MySQL:
    * Remove reports more than 4 weeks old
    * Clean up orphaned entries
    * Optimize MySQL database
    
    Change-Id: I8bc075e14dad48922b6979a38fb31ad829ce541b

---
+++ modules/traf/files/dashboard/purgeDashboardDB.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_NAME=`basename $0`
+DASHBOARD_LOCK="/usr/share/puppet-dashboard/tmp/pids/purge-dashboard.lock"
+
+# file lock to ensure only one is running at any given time
+exec 7>$DASHBOARD_LOCK
+if flock -n 7
+then
+    pid=$$
+    echo $pid 1>&7
+    
+    # clean up dashboard database
+    cd $DASHBOARD_DIR
+    sudo -u www-data rake RAILS_ENV=production reports:prune upto=4 unit=wk     # remove reports
+    sudo -u www-data rake RAILS_ENV=production reports:prune:orphaned           # remove orphans
+    sudo -u www-data rake RAILS_ENV=production db:raw:optimize                  # optimize database
+else
+    echo "Cannot acquire file lock. Script $SCRIPT_NAME is already running on pid " `cat $DASHBOARD_LOCK`
+    exit 1
+fi
--- modules/traf/manifests/dashboard.pp
+++ modules/traf/manifests/dashboard.pp
@@ -1,6 +1,7 @@
 class traf::dashboard(
     $password = '',
     $mysql_password = '',
+    $mysql_innodb_buffer_pool_size = '1536M',
     $sysadmins = []
 ) {
 
@@ -22,6 +23,25 @@ class traf::dashboard(
     passenger           => true,
   }
 
+  # Tune MySQL max_allowed_packet to 32M or greater
+  exec { 'up mysql max_allowed_packet':
+    cwd     => "/etc/mysql",
+    command => "/bin/sed -i.bak -e 's/^max_allowed_packet =.*/max_allowed_packet = 32M/g' my.cnf",
+    unless  => "/bin/grep -E '^max_allowed_packet = ([3456789][23456789]|[1-9][0-9]{2,})M' my.cnf",
+    require => Class['mysql::server'],
+    notify  => Service[mysql],
+  }
+
+  file { '/etc/mysql/conf.d/mysqld_innodb_dashboard.cnf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('traf/mysqld_innodb_dashboard.cnf.erb'),
+    require => Class['mysql::server'],
+    notify  => Service[mysql],
+  }
+
   file { '/etc/mysql/conf.d/mysqld_innodb_fpt.cnf':
     ensure  => present,
     source  => 'puppet:///modules/openstack_project/dashboard/mysqld_innodb_fpt.cnf',
@@ -32,13 +52,46 @@ class traf::dashboard(
     ensure  => file,
     source  => 'puppet:///modules/traf/dashboard/puppet-dashboard-workers',
     require => Class['::dashboard'],
-    notify  => Service[puppet-dashboard-workers]
+    notify  => Service[puppet-dashboard-workers],
   }
 
   service { 'puppet-dashboard-workers':
-      ensure  => running,
-      require => File['/etc/default/puppet-dashboard-workers'],
-      subscribe  => File['/etc/default/puppet-dashboard-workers'],
+    ensure     => running,
+    require    => File['/etc/default/puppet-dashboard-workers'],
+    subscribe  => File['/etc/default/puppet-dashboard-workers'],
+  }
+
+  # patch /usr/share/puppet-dashboard/lib/tasks/prune_reports.rake to clean up resource_status table
+  file { '/usr/share/puppet-dashboard/lib/tasks/prune_reports.rake.patch':
+    source => 'puppet:///modules/traf/dashboard/prune_reports.rake.patch',
+    notify => Exec['Apply prune_reports.rake.patch'],
+  }
+
+  exec { 'apply prune_reports.rake.patch':
+    cwd         => "/usr/share/puppet-dashboard/lib/tasks",
+    command     => "/usr/bin/patch -p0 -N < prune_reports.rake.patch",
+    onlyif      => "/usr/bin/patch -p0 -N --dry-run --silent < prune_reports.rake.patch 2>/dev/null",
+    refreshonly => true,
+  }
+
+  # create cronjob to cleanup the MySQL database
+  file { '/usr/share/puppet-dashboard/bin/purgeDashboardDB.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/traf/dashboard/purgeDashboardDB.sh',
+    require => File['/usr/share/puppet-dashboard/bin'],
+  }
+
+  cron { 'purge-dashboard-db': 
+    environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
+    command     => 'bash /usr/share/puppet-dashboard/bin/purgeDashboardDB.sh',
+    user        => 'root',
+    hour        => '23',
+    minute      => '0',
+    ensure      => present,
+    require     => File['/usr/share/puppet-dashboard/bin/purgeDashboardDB.sh'],
   }
 }
 
