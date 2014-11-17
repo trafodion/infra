9247f78e7756f2714c14fdbb8329e59278cea547
Steve Varnau <steve.varnau@hp.com>
2014-11-17 10:56:17 -0800

    Avoid removing winbld directory in log clean-up
    This is a staging directory for windows build, not a log directory.
    
    Change-Id: I99a548c8c418fac17d0a72d0ee6da0408ef10fbc

--- modules/traf/files/log_archive_maintenance.sh
+++ modules/traf/files/log_archive_maintenance.sh
@@ -10,6 +10,7 @@ find -O3 /srv/static/logs/ -depth \
       \( -type f -mtime +3 -not -name robots.txt \
           -not -wholename /srv/static/logs/help/\* \
           -not -wholename /srv/static/logs/buildvers/\* \
+          -not -wholename /srv/static/logs/winbld/\* \
           -not -name \*\[.-\]gz -not -name \*\[._-\]\[zZ\] \
           -exec gzip -f \{\} \; \) \
       -o \( -type f -mtime +14 -name \*.gz -execdir rm \{\} \; \) \
