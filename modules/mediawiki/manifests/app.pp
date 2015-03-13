ad22b21537a1fe138e024fb73218acf4a2b75a72
Steve Varnau <steve.varnau@hp.com>
2015-03-12 17:21:18 -0700

    Updated wiki repo includes extension config updates
    Change-Id: Ibce23744950a2832c0cdab260ae279bb5f841a41

--- modules/mediawiki/manifests/app.pp
+++ modules/mediawiki/manifests/app.pp
@@ -5,7 +5,7 @@ class mediawiki::app {
     ensure   => present,
     provider => git,
     source   => 'https://gerrit.wikimedia.org/r/p/mediawiki/core.git',
-    revision => 'e013ed02343d4274a2e12e074a4d016772082e1e',  # temp local branch, See LP bug 1430550
+    revision => 'e01f8d3c948fac6c44e06471822427d7d00df88a',  # temp local branch, See LP bug 1430550
     owner    => 'www-data',
     group    => 'www-data',
   }
