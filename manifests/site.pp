0b74acfc8afa30635eaf3351f5c82117fdb1e4bf
Alice Chen <alice.chen@hp.com>
2015-05-02 01:01:58 +0000

    Added cloud_auto_tenant_id which is needed by useObjectStorage.sh
    Change-Id: I65b7dfd1feaa80fe47afc325be9daef8cca86610

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,7 @@
 
 $cloud_auto_user=hiera('cloud_auto_user', '')
 $cloud_auto_passwd=hiera('cloud_auto_passwd', '')
+$cloud_auto_tenant_id=hiera('cloud_auto_tenant_id', '')
 
 # default puppet-master
 $pserver='puppet3.trafodion.org'
