787b63fe6d659404d0db30b48e8d5f071d5796c2
Steve Varnau <steve.varnau@hp.com>
2015-06-29 15:20:05 -0700

    Install email-ext plugin
    This will allow customized email notification of daily builds.

--- modules/traf/manifests/jenkins.pp
+++ modules/traf/manifests/jenkins.pp
@@ -130,11 +130,8 @@ class traf::jenkins (
   jenkins::plugin { 'envinject':
     version => '1.90',
   }
-  jenkins::plugin { 'zmq-event-publisher':
-    version => '0.0.3',
-  }
-  jenkins::plugin { 'gearman-plugin':
-    version => '0.1.1',
+  jenkins::plugin { 'email-ext':
+    version => '2.40.5',
   }
   jenkins::plugin { 'git':
     version => '2.3.5',
