91f089fad5ce9bf9fbf6dbb51eb0d61379ff8e1a
Steve Varnau <steve.varnau@esgyn.com>
2015-11-25 17:41:50 +0000

    change static vhost templates to new module format

--- modules/traf/manifests/static.pp
+++ modules/traf/manifests/static.pp
@@ -19,13 +19,20 @@ class traf::static (
   }
 
 
-  include apache
-  #include apache::mod::wsgi
+  class { 'apache':
+   server_tokens => 'Prod',
+  }
+  class { 'apache::mod::event':
+   maxclients             => '75',
+   maxconnectionsperchild => '600',
+  }
+  include apache::mod::wsgi
   include apache::mod::expires
   include apache::mod::headers
   include apache::mod::rewrite
   include apache::mod::proxy
   include apache::mod::proxy_http
+  include apache::mod::deflate
 
 
   # make sure Curl and PHP is installed
@@ -43,11 +50,13 @@ class traf::static (
   # www -- send them to Apache trafodion project page
 
   apache::vhost { 'www.trafodion.org':
-    port          => 80,
-    priority      => '01',
-    docroot       => $server_path,
-    serveraliases => ['trafodion.org', 'www.trafodion.com', 'trafodion.com', '15.125.67.182'],
-    #template      => 'traf/www.vhost.erb',
+    port            => 80,
+    priority        => '01',
+    docroot         => $server_path,
+    serveraliases   => ['trafodion.org', 'www.trafodion.com', 'trafodion.com', '15.125.67.182'],
+    redirect_source => '/',
+    redirect_status => 'permanent',
+    redirect_dest   => 'http://trafodion.incubator.apache.org/',
   }
 
 
@@ -57,12 +66,13 @@ class traf::static (
 
   # testing "traf" -> "t2"
   apache::vhost { 't2-downloads.esgyn.com':
-    serveraliases  => "downloads.trafodion.org",
-    port           => 80,
-    priority       => '50',
-    docroot        => "${server_path}/downloads-www",
-    template       => 'traf/downloads/downloads.vhost.erb',
-    require        => File["${server_path}/downloads-www"],
+    serveraliases    => "downloads.trafodion.org",
+    port             => 80,
+    priority         => '50',
+    docroot          => "${server_path}/downloads-www",
+    server_signature => 'Off',
+    setenv           => ['no-gzip dont-vary'],
+    require          => File["${server_path}/downloads-www"],
   }
 
   # where download site resides
@@ -113,26 +123,26 @@ class traf::static (
   #  require => File["${server_path}/downloads-www"],
   #}
 
-  file { "${server_path}/downloads-www/common.js":
-    ensure  => present,
-    source  => 'puppet:///modules/traf/status/common.js',
-    require => File["${server_path}/downloads-www"],
-  }
-
-  file { "${server_path}/downloads-www/themes":
-    ensure  => link,
-    target  => "${server_path}/themes",
-    require => [File["${server_path}/downloads-www"],
-                File["${server_path}/themes"]],
-  }
-
-  exec { 'get-httpful-phar':
-    command => "/usr/bin/curl http://phphttpclient.com/downloads/httpful.phar > ${server_path}/downloads-www/lib/httpful.phar",
-    cwd     => "${server_path}/downloads-www/lib",
-    creates => "${server_path}/downloads-www/lib/httpful.phar",
-    require => [Package['curl'],
-                File["${server_path}/downloads-www/lib"]],
-  }
+#  file { "${server_path}/downloads-www/common.js":
+#    ensure  => present,
+#    source  => 'puppet:///modules/traf/status/common.js',
+#    require => File["${server_path}/downloads-www"],
+#  }
+#
+#  file { "${server_path}/downloads-www/themes":
+#    ensure  => link,
+#    target  => "${server_path}/themes",
+#    require => [File["${server_path}/downloads-www"],
+#                File["${server_path}/themes"]],
+#  }
+#
+#  exec { 'get-httpful-phar':
+#    command => "/usr/bin/curl http://phphttpclient.com/downloads/httpful.phar > ${server_path}/downloads-www/lib/httpful.phar",
+#    cwd     => "${server_path}/downloads-www/lib",
+#    creates => "${server_path}/downloads-www/lib/httpful.phar",
+#    require => [Package['curl'],
+#                File["${server_path}/downloads-www/lib"]],
+#  }
 
   # actual location of files to download
   file { $download_path:
@@ -150,17 +160,6 @@ class traf::static (
     require => [
       User['jenkins'],
       File[$download_path],
-    ]
-  }
-
-  file { "${download_path}/trafodion/publish":
-    ensure  => directory,
-    owner   => 'jenkins',
-    group   => 'jenkins',
-    mode    => '0775',
-    require => [
-      User['jenkins'],
-      File["${download_path}/trafodion"],
     ]
   }
 
@@ -191,20 +190,20 @@ class traf::static (
   ###########################################################
   # Docs
 
-  apache::vhost { 'docs.trafodion.org':
-    port     => 80,
-    priority => '50',
-    docroot  => "${server_path}/docs",
-    template => 'traf/docs/docs.vhost.erb',
-    require  => File["${server_path}/docs"],
-  }
-
-  file { "${server_path}/docs":
-    ensure  => directory,
-    owner   => 'jenkins',
-    group   => 'jenkins',
-    require => User['jenkins'],
-  }
+#  apache::vhost { 'docs.trafodion.org':
+#    port     => 80,
+#    priority => '50',
+#    docroot  => "${server_path}/docs",
+#    template => 'traf/docs/docs.vhost.erb',
+#    require  => File["${server_path}/docs"],
+#  }
+#
+#  file { "${server_path}/docs":
+#    ensure  => directory,
+#    owner   => 'jenkins',
+#    group   => 'jenkins',
+#    require => User['jenkins'],
+#  }
 
 
   ###########################################################
@@ -212,12 +211,33 @@ class traf::static (
 
   # "traf" -> "t2"
   apache::vhost { 't2-logs.esgyn.com':
-    serveraliases  => "logs.trafodion.org",
-    port           => 80,
-    priority       => '50',
-    docroot        => "${server_path}/logs",
-    require        => File["${server_path}/logs"],
-    template       => 'traf/logs.vhost.erb',
+    serveraliases       => "logs.trafodion.org",
+    port                => 80,
+    priority            => '50',
+    docroot             => "${server_path}/logs",
+    server_signature    => 'Off',
+    directories         => [
+      { path                => '\.html\.gz$',
+        provider            => 'filesmatch',
+	force_type          => 'text/html',
+	add_default_charset => 'UTF-8',
+	custom_fragment     => 'AddEncoding x-gzip gz',
+      },
+      { path                => "${server_path}/logs",
+        provider            => 'directory',
+	override            => ['None'],
+	order               => 'Allow,Deny',
+	allow               => 'from all',
+      },
+    ],
+    rewrites            => [
+      { comment             => 'rewrite all txt.gz & html.gz files to map to our internal htmlify wsgi app',
+        rewrite_rule        => ['^/(.*\.txt\.gz)$ /htmlify/$1 [QSA,L,PT]',
+                                '^/(.*console\.html(\.gz)?)$ /htmlify/$1 [QSA,L,PT]'],
+      },
+    ],
+    wsgi_script_aliases => { '/htmlify' => '/usr/local/lib/python2.7/dist-packages/os_loganalyze/wsgi.py' },
+    require             => File["${server_path}/logs"],
   }
 
   file { "${server_path}/logs":
@@ -260,95 +280,79 @@ class traf::static (
   ###########################################################
   # www
 
-  file { "${server_path}/themes":
-    ensure  => directory,
-    mode    => '0755',
-    recurse => true,
-  }
+#  file { "${server_path}/themes":
+#    ensure  => directory,
+#    mode    => '0755',
+#    recurse => true,
+#  }
+#
+#  file { "${server_path}/themes/trafodion":
+#    ensure  => directory,
+#    mode    => '0755',
+#    recurse => true,
+#    require => File["${server_path}/themes"],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/images":
+#    ensure  => directory,
+#    mode    => '0755',
+#    recurse => true,
+#    source  => 'puppet:///modules/traf/images/',
+#    require => File["${server_path}/themes/trafodion"],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/images/Trafodion.png":
+#    ensure  => present,
+#    source  => 'puppet:///modules/traf/Trafodion.png',
+#    require => File["${server_path}/themes/trafodion/images"],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/css":
+#    ensure  => directory,
+#    mode    => '0755',
+#    recurse => true,
+#    require => File["${server_path}/themes/trafodion"],
+#  }
+#
+#  vcsrepo { '/opt/blueprint-css':
+#    ensure   => latest,
+#    provider => git,
+#    revision => 'master',
+#    source   => 'https://github.com/joshuaclayton/blueprint-css.git',
+#  }
+#
+#  file { "${server_path}/themes/trafodion/css/blueprint":
+#    ensure  => link,
+#    target  => '/opt/blueprint-css/blueprint',
+#    require => [File["${server_path}/themes/trafodion/css"],
+#                Vcsrepo['/opt/blueprint-css']],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/css/dropdown.css":
+#    ensure  => present,
+#    source  => 'puppet:///modules/traf/css/dropdown.css',
+#    require => File["${server_path}/themes/trafodion/css"],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/css/home.css":
+#    ensure  => present,
+#    source  => 'puppet:///modules/traf/css/home.css',
+#    require => File["${server_path}/themes/trafodion/css"],
+#  }
+#
+#  file { "${server_path}/themes/trafodion/css/main.css":
+#    ensure  => present,
+#    source  => 'puppet:///modules/traf/css/main.css',
+#    require => File["${server_path}/themes/trafodion/css"],
+#  }
 
-  file { "${server_path}/themes/trafodion":
-    ensure  => directory,
-    mode    => '0755',
-    recurse => true,
-    require => File["${server_path}/themes"],
-  }
 
-  file { "${server_path}/themes/trafodion/images":
-    ensure  => directory,
-    mode    => '0755',
-    recurse => true,
-    source  => 'puppet:///modules/traf/images/',
-    require => File["${server_path}/themes/trafodion"],
-  }
-
-  file { "${server_path}/themes/trafodion/images/Trafodion.png":
-    ensure  => present,
-    source  => 'puppet:///modules/traf/Trafodion.png',
-    require => File["${server_path}/themes/trafodion/images"],
-  }
-
-  file { "${server_path}/themes/trafodion/css":
-    ensure  => directory,
-    mode    => '0755',
-    recurse => true,
-    require => File["${server_path}/themes/trafodion"],
-  }
-
-  vcsrepo { '/opt/blueprint-css':
-    ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://github.com/joshuaclayton/blueprint-css.git',
-  }
-
-  file { "${server_path}/themes/trafodion/css/blueprint":
-    ensure  => link,
-    target  => '/opt/blueprint-css/blueprint',
-    require => [File["${server_path}/themes/trafodion/css"],
-                Vcsrepo['/opt/blueprint-css']],
-  }
-
-  file { "${server_path}/themes/trafodion/css/dropdown.css":
-    ensure  => present,
-    source  => 'puppet:///modules/traf/css/dropdown.css',
-    require => File["${server_path}/themes/trafodion/css"],
-  }
-
-  file { "${server_path}/themes/trafodion/css/home.css":
-    ensure  => present,
-    source  => 'puppet:///modules/traf/css/home.css',
-    require => File["${server_path}/themes/trafodion/css"],
-  }
-
-  file { "${server_path}/themes/trafodion/css/main.css":
-    ensure  => present,
-    source  => 'puppet:///modules/traf/css/main.css',
-    require => File["${server_path}/themes/trafodion/css"],
-  }
-
-  # update apache2 security configuration
-  exec { 'update security':
-    cwd     => '/etc/apache2/conf.d',
-    command => "/bin/sed -i -e 's/^ServerTokens .*/ServerTokens Prod/g' security",
-    unless  => "/bin/grep -E '^ServerTokens Prod' security",
-    notify  => Service[httpd],
-  }
-
-  # update apache2 configuration
-  # configure MaxClients and MaxRequestsPerChild for 4GB server
-  exec { 'update apache2':
-    cwd     => '/etc/apache2',
-    command => "/bin/sed -i -e 's/^    MaxClients .*/    MaxClients 75/g' -e 's/^    MaxRequestsPerChild .*/    MaxRequestsPerChild 600/g' apache2.conf",
-    unless  => "/bin/grep -E '^    MaxClients 75' apache2.conf && /bin/grep -E '^    MaxRequestsPerChild 600' apache2.conf",
-    notify  => Service[httpd],
-  }
-
-  # update php.ini configuration
-  exec { 'update php.ini':
-    cwd     => '/etc/php5/apache2',
-    command => "/bin/sed -i -e 's/^expose_php = .*/expose_php = Off/g' -e 's/^engine = .*/engine = Off/g' php.ini",
-    unless  => "/bin/grep -E '^expose_php = Off' php.ini && /bin/grep -E '^engine = Off' php.ini",
-    notify  => Service[httpd],
-  }
+#  # update php.ini configuration
+#  exec { 'update php.ini':
+#    cwd     => '/etc/php5/apache2',
+#    command => "/bin/sed -i -e 's/^expose_php = .*/expose_php = Off/g' -e 's/^engine = .*/engine = Off/g' php.ini",
+#    unless  => "/bin/grep -E '^expose_php = Off' php.ini && /bin/grep -E '^engine = Off' php.ini",
+#    notify  => Service[httpd],
+#  }
 
 }
