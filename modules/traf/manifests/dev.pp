b8ec76cc545f17118712594171b66385c31137b2
Steve Varnau <steve.varnau@esgyn.com>
2016-02-03 17:58:28 +0000

    Add gnuplot for dbmgr dependency

--- modules/traf/manifests/dev.pp
+++ modules/traf/manifests/dev.pp
@@ -74,7 +74,7 @@ class traf::dev (
   }
 
 
-  package { ['emacs','gitk','gedit','kdesdk','firefox','vim-enhanced','xterm']:
+  package { ['emacs','gitk','gedit','kdesdk','firefox','vim-enhanced','xterm','gnuplot']:
     ensure => present,
   }
   # work-around for group install
